229ad03cbc55c8112b7ffc782bf52c9438ffc9d5
Franco Moyano <koprammoyano@gmail.com>
2022-05-03 12:25:52 -0300

    Added: Angular Material

--- oeste-neumaticos-front/src/app/app.module.ts
+++ oeste-neumaticos-front/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaNeumaticosComponent } from './lista-neumaticos/lista-neumaticos.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaNeumaticosComponent
+    ListaNeumaticosComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
